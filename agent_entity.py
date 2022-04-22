fc9b5ef245a659d44c78dc49830a773740d5fcbf
dukl <du_keliang@163.com>
2022-04-22 09:02:16 +0800

    add DQN model

--- agent_entity.py
+++ agent_entity.py
@@ -1,6 +1,9 @@
 import random
 
+import numpy as np
+
 from logger import log
+from DQN_Model.RL_brain import DeepQNetwork
 
 class Agent:
     def __init__(self):
@@ -8,15 +11,27 @@ class Agent:
         self.history_obs = []
         self.history_acts = []
         self.history_rwds = []
+        self.model = DeepQNetwork(
+            3, 25, learning_rate=0.01, reward_decay=0.9, e_greedy=0.9, replace_target_iter=200, memory_size=2000
+        )
+        self.step = 0
+        self.pending_state = None
+        self.pending_action = None
 
     def receive_observation(self, obs, delta_t):
         if len(obs) > 0:
             for i in range(len(obs)):
                 log.logger.debug('[Agent][receives s['+ str(obs[i].id)+']] = '+''.join(str(obs[i].value)))
-                #self.history_rwds.append()
-            self.history_obs.append(obs[0]) # store latest observation
-            return self.action_space[random.randint(1,3)-1]
-            #return 1
+            self.step += 1
+            if (self.step > 200) and (self.step % 5 ==0):
+                self.model.learn()
+            if self.pending_action is not None:
+                self.model.store_transition(self.pending_state, self.pending_action, obs[0].reward.value, np.array(obs[0].value).reshape(1,25))
+            self.pending_state = np.array(obs[0].value).reshape(1, 25)
+            action = self.model.choose_action(self.pending_state)
+            self.pending_action = action
+
+            return self.action_space[action]
         else:
             log.logger.debug('[Agent][does not receive any observation at this time point]')
             return None
